73c7ffc8ee76b2f5786388157b9bd393f3b1a40d
Khushpreet Singh <146699139+khushprn@users.noreply.github.com>
2024-04-07 22:01:41 +0300

    Delete program2.py

--- program2.py
+++
@@ -1,8 +0,0 @@
-inches = 2.54
-i = float(input("Enter inches to convert them into centimetres: "))
-while i >= 0:
-    res = inches*i
-    print(res)
-    i = float(input("Enter another value of inches or enter negetive number to end this program"))
-else:
-    print("End program")
